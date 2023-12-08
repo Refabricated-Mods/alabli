bd2ae0a1fd2ba06f790ccc99fc236e55a12b794e
Unix <unix-supremacist@users.noreply.github.com>
2023-12-08 15:02:27 -0600

    blocktag

--- src/main/java/io/github/unix_supremacist/alabli/AlabliRegistry.java
+++ src/main/java/io/github/unix_supremacist/alabli/AlabliRegistry.java
@@ -4,8 +4,11 @@ import io.github.unix_supremacist.Alchemist;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+
+import java.util.ArrayList;
 
 public class AlabliRegistry {
     public static void registerItem(Item item, String name){
@@ -15,5 +18,9 @@ public class AlabliRegistry {
 
     public static void registerBlock(Block block, String name){
         Registry.register(BuiltInRegistries.BLOCK, new ResourceLocation(Alchemist.MODID, name), block);
+    }
+
+    public static void registerBlockTag(TagKey<Block> tag, ArrayList<Block> arrayList){
+        BuiltInRegistries.BLOCK.getTagOrEmpty(tag).iterator().forEachRemaining(t ->arrayList.add(t.value()));
     }
 }
