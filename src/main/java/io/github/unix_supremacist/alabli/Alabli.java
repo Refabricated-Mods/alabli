f01ccc98713cb1c0582dffaad79b09ad2da13a3f
Unix <unix-supremacist@users.noreply.github.com>
2023-12-07 19:35:38 -0600

    blockproperties

--- src/main/java/io/github/unix_supremacist/alabli/Alabli.java
+++ src/main/java/io/github/unix_supremacist/alabli/Alabli.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
@@ -14,6 +15,8 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,6 +26,7 @@ import java.util.List;
 public class Alabli implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(Alchemist.MODID);
 	public static ArrayList<Item> items = new ArrayList<>();
+	public static BlockBehaviour.Properties properties = FabricBlockSettings.create();
 	public static final CreativeModeTab tab = FabricItemGroup.builder()
 			.icon(() -> new ItemStack(AlchemistItems.philosophers_stone.getItem()))
 			.title(Component.translatable("itemGroup."+Alchemist.MODID))
